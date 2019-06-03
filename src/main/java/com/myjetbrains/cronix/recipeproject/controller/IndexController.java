9e73f5dc73e3e0464028129a2e7e0addb75b8b1c
cronix <grzegorz.chron@gmail.com>
2019-06-03 14:26:42 +0200

    initial commit

---
+++ src/main/java/com/myjetbrains/cronix/recipeproject/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.myjetbrains.cronix.recipeproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"/","","/index","/index.html"})
+    public String getIndexPage() {
+
+        return "index";
+    }
+}
